0ac94a9ebc058e43e09216f0354b75bc479fb4fa
GUSTAVO MIGUEL DOS SANTOS PEREIRA <24122458@dsn127m1246732.cfp127aluno.local>
2025-02-20 12:12:03 -0300

    tela inicial ok

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -0,0 +1,140 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF3F2621),
+                        Color(0xDF8D6504)
+                    )
+                )
+            )
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = painterResource(
+                    R.drawable.workout
+                ),
+                contentDescription = stringResource(
+                    R.string.logo
+                ),
+                modifier = Modifier
+                    .padding(top = 25.dp)
+            )
+            Text(
+                text = stringResource(
+                    R.string.welcome
+                ),
+                color = Color.White,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp),
+                    shape = RoundedCornerShape(
+                        topStart = 48.dp,
+                        topEnd = 48.dp
+
+                    ),
+                colors = CardDefaults
+                    .cardColors(
+                        containerColor = Color.White
+                    )
+
+
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.End
+
+
+
+                ) { Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+
+
+                ) {
+
+                    Text(
+                        text = stringResource(
+                            R.string.your_name
+                        ),
+                        color = Color.Black,
+                        fontSize = 20.sp
+                    )
+
+                    TextField(value = "", onValueChange = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 6.dp)
+                    )
+                }
+                    Button(onClick = {},
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(
+                            text = stringResource(
+                                R.string.next
+
+                            ),
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
