a06fe3ac41cb93b6bedd0f1c95e1af4695e4f6e2
GUSTAVO MIGUEL DOS SANTOS PEREIRA <24122458@dsn127m1246732.cfp127aluno.local>
2025-03-06 09:31:53 -0300

    textfield ok

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -11,12 +11,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -24,6 +27,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,6 +36,11 @@ import br.senai.sp.jandira.bmi.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+
+    var nameState = remember {
+        mutableStateOf("")
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -38,7 +48,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 brush = Brush.horizontalGradient(
                     listOf(
                         Color(0xFF3F2621),
-                        Color(0xDF8D6504)
+                        Color(0xDF8A6B21)
                     )
                 )
             )
@@ -108,10 +118,18 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                         fontSize = 20.sp
                     )
 
-                    TextField(value = "", onValueChange = {},
+                    TextField(
+                        value = nameState.value,
+                        onValueChange = {
+                        nameState.value = it
+                    },
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 6.dp)
+                            .padding(top = 6.dp),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Words
+                        )
                     )
                 }
                     Button(onClick = {},
